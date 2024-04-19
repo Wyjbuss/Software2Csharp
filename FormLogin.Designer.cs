01640ae76e0e0f55bea277c436b153f30484f353
Wyatt <wyjbuss@gmail.com>
2024-04-19 20:07:55 +0000

    added inital project, change laguage on login screen will function

---
+++ FormLogin.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Software2Csharp
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.domainUpDownLocation = new System.Windows.Forms.DomainUpDown();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(420, 155);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(33, 13);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "Login";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(386, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(386, 217);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(276, 194);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(58, 13);
+            this.labelUsername.TabIndex = 3;
+            this.labelUsername.Text = "Username:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(276, 220);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(56, 13);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Password:";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(411, 260);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 5;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // domainUpDownLocation
+            // 
+            this.domainUpDownLocation.AllowDrop = true;
+            this.domainUpDownLocation.Items.Add("English");
+            this.domainUpDownLocation.Items.Add("Spanish");
+            this.domainUpDownLocation.Location = new System.Drawing.Point(796, 12);
+            this.domainUpDownLocation.Name = "domainUpDownLocation";
+            this.domainUpDownLocation.ReadOnly = true;
+            this.domainUpDownLocation.Size = new System.Drawing.Size(120, 20);
+            this.domainUpDownLocation.TabIndex = 6;
+            this.domainUpDownLocation.Text = "domainUpDown1";
+            this.domainUpDownLocation.SelectedItemChanged += new System.EventHandler(this.domainUpDownLocation_SelectedItemChanged);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 500);
+            this.Controls.Add(this.domainUpDownLocation);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelLogin);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.DomainUpDown domainUpDownLocation;
+    }
+}
+
---
+++ FormLogin.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Configuration;
+
+namespace Software2Csharp
+{
+    public partial class FormLogin : Form
+    {
+        string locationNameContryCode;
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            // returns the 2 letter country code
+            locationNameContryCode = RegionInfo.CurrentRegion.Name;
+            //RegionInfo reginInfo = new RegionInfo("US");
+            domainUpDownLocation.Text = "Spanish";
+            if (locationNameContryCode != null || locationNameContryCode != "Undefined")
+            {
+                // execute this if we have a region
+                Console.WriteLine("The regin that you are in is: " + locationNameContryCode);
+                convertRegionToLanguage();
+
+            }
+            else { Console.WriteLine("Region is not fround on form load. Default to US."); locationNameContryCode = "US"; }
+        }
+
+        private void convertRegionToLanguage()
+        {
+            if (locationNameContryCode == "US" )
+            {
+                domainUpDownLocation.Text = "English";
+                englishLanguage();
+                
+            }
+            else if (locationNameContryCode == "MX" )
+            {
+                domainUpDownLocation.Text = "Spanish";
+                spanishLanguage();
+            }
+        }
+        private void englishLanguage()
+        {
+            labelLogin.Text = "Login";
+            labelPassword.Text = "Password:";
+            labelUsername.Text = "Username:";
+            buttonLogin.Text = "Login";
+        }
+        private void spanishLanguage()
+        {
+            labelLogin.Text = "Acceso";
+            labelPassword.Text = "Contraseña:";
+            labelUsername.Text = "Nombre de usuario:";
+            buttonLogin.Text = "Acceso";
+        }
+
+        private void domainUpDownLocation_SelectedItemChanged(object sender, EventArgs e)
+        {
+            if (domainUpDownLocation.Text == "English")
+            {
+                englishLanguage();
+            }
+            else if (domainUpDownLocation.Text == "Spanish")
+            {
+                spanishLanguage();
+            }
+        }
+    }
+}
