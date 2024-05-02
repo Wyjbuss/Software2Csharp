5ed6c54f7a8024610180768d45bfba8414f628c8
Wyatt Bussell <wyjbuss@gmail.com>
2024-05-02 22:09:42 +0000

    added classes appointments and customers

---
+++ ClassAppointments.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2Csharp
+{
+    internal class ClassAppointments
+    {
+        public enum appointmentType { }
+        public string title;
+        public string description;
+        public string location;
+        public DateTime startTime;
+        public DateTime endTime;
+        public DateTime lastUpdated;
+        public string lastUpdaedBy;
+        public DateTime createdDate;
+        public string createdBy;
+        public string url;
+
+        // link to customer
+
+        //get current appointments and prevent from setting one that overlaps
+        public ClassAppointments() 
+        {
+            title = "newAppointmentTitle" + 1;
+        }
+
+        public static void addAppointment(ClassAppointments appointment)
+        {
+            // create database entry and add the data in this appointment 
+        }
+
+        public static void removeAppointment(int AppointmentID) 
+        {
+
+        }
+
+        public static void updateAppointment(int AppointmentID) 
+        {
+            // get the current appointment by ID and update the data
+        }
+    }
+
+   
+}
---
+++ ClassCustomers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2Csharp
+{
+    internal class ClassCustomers
+    {
+        public string customerName;
+        public bool active;
+        public DateTime createdDate;
+        public DateTime updatedDate;
+        public string createdBy;
+        public string updatedBy;
+        public ClassCustomers() 
+        {
+            customerName = "New Customer" + 1;
+            active = true;
+            createdDate = DateTime.Now;
+            updatedDate = DateTime.Now;
+        }
+
+        public static void addCustomer(ClassCustomers newCustomer)
+        {
+
+        }
+        public static void removeCustomer(int customerID)
+        {
+
+        }
+        public static void updateCustomer(int customerID)
+        {
+
+        }
+    }
+
+}
